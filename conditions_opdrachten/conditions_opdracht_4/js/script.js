6a0262dde4db29019838268d32f1c292cd22ec9b
Nour Salama <2107455@talnet.nl>
2019-11-26 13:21:56 +0100

    Opdracht 4 en 5 afgerond

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,4 @@
-//javascript code
+var myNumber ;
+
+myNumber= 87965;
+alert("De variablele my number heeft de waarde : " +  myNumber);
--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,28 @@
-//javascript code
+var myOperator = "-";
+var myNumber1 = prompt("geef aub een getal op?");
+var myNuber2 = prompt("geef aub een getal op?");
+var resultaat ;
+
+if(myOperator == "+"){
+    
+    
+    resultaat = myNumber1 + myNuber2;
+    document.getElementById(resultaat).innerHTML = resultaat;
+
+    
+    
+    
+    // hier komt code 
+    // console.log("De operator is  " + myOperator);
+
+
+
+} else if(myOperator == "-") {
+    
+    resultaat = myNumber1 - myNuber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+    
+    // Hier komt een code
+    // console.log("De operator is  " - myOperator);
+
+}
