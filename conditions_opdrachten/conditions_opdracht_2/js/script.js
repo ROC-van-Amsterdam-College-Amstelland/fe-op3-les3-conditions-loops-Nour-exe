24f7d64d1e8d8e2b521c224d757a7b368e04399a
Nour Salama <2107455@talnet.nl>
2019-11-26 11:08:55 +0100

    Opdracht 2 af

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,18 @@
-//javascript code
+function checkSaldo() {
+
+    var geldOpname= document.getElementById("CheckSaldo").value;
+
+    alert (geldOpname); 
+
+
+    var bankSaldo = 1000;
+
+    var restTotaal = 1000 - geldOpname;
+
+
+    if(restTotaal > 25) {
+    alert("sorry je hebt niet genoeg, geld op je rekening");
+    }
+
+
+    }
