2dc8f1d8374e4cad77471a9c10a91a170601a6bc
Nour Salama <2107455@talnet.nl>
2019-11-26 10:52:54 +0100

    Opdracht 1 af.

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,13 @@
-//javascript code
+// function SaldoChecker() {
+    var geld = prompt ("Hoe veel geld wil je pinnen?")
+    var banksaldo= 50;
+
+    if (geld < 25 ) {
+        alert ("sorry , je hebt te weinig geld"); 
+    
+    }
+    
+
+    
+
+    
