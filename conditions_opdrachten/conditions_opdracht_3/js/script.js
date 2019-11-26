64f9633e7a5c26e7dec606be5cc1fca60fab5fd2
Nour Salama <2107455@talnet.nl>
2019-11-26 11:37:20 +0100

    Opdracht 3 Afgerond

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,21 @@
-//javascript code
+function checkSaldo() {
+
+    var geldOpname= document.getElementById("CheckSaldo").value;
+
+    alert (geldOpname); 
+
+
+    var bankSaldo = 500;
+
+   if(bankSaldo == 500) {
+    alert("Je salari is gestort !");
+    }
+    else
+    
+    if(bankSaldo > bankSaldo);{
+    alert ("Je hebt een bonus ontavangen")
+
+    }
+
+
+    }
