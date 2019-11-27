d44f1e8556ced40bbc241a641f1aadeb542af80c
Nour Salama <2107455@talnet.nl>
2019-11-27 10:55:55 +0100

    Alles af t/m opdracht 6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,25 @@
-//javascript code
+var myOperator = prompt("Vier de waarde in van je rekensom")
+var myNumber1 = prompt("geef aub een getal op?");
+var myNuber2 = prompt("geef aub een getal op?");
+var resultaat ;
+
+if(myOperator = "*"){
+    
+    
+    resultaat = myNumber1 * myNuber2;
+    document.getElementById(resultaat).innerHTML = resultaat;
+
+    
+    
+    
+  
+
+
+} else if(myOperator = "/") {
+    
+    resultaat = myNumber1 / myNuber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+    
+
+
+}
--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,14 @@
-//javascript code
+/* 
+A)	Alle getallen toon je onder elkaar op het scherm via index.html. <br>
+B)	Alle getallen toon je met behulp van console.log() in je console */
+/* 
+for(statement 1; statement 2; statement 3;){
+
+teller = teller + 1 ;
+teller += 1;
+} */
+
+for(var teller = 1 ; teller <= 5 ; teller ++){
+    console.log(teller);
+    document.getElementById("resultaat").innerHTML += teller
+}
