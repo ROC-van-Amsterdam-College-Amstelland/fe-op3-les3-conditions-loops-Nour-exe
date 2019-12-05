a09debbe692f4cf198add1bbc5eeae3b5e91f8fe
Nour Salama <2107455@talnet.nl>
2019-12-05 14:54:32 +0100

    Loops Gezamelijk Gemaakt

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,34 @@
-//javascript code
+
+// var number= 1;
+// nummer = number + 1;
+// nummer += 1;
+// nummer ++;
+// 
+
+
+
+
+for( var teller= 1 ; teller <= 20 ; teller++ ) {
+    console.log(teller);
+    
+    console.log (teller % 2 );
+    
+    if( teller % 2 == 0){
+    document.getElementById("resultaat").innerHTML  +=  "<h6>" + teller + "</h6>";
+    }
+    else{
+        console.log(teller);
+    }
+
+
+}
+
+// if() {
+
+
+// }
+
+// function(){
+
+
+// }
