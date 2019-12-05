17b6954a6963cef032dcd133a63060dbf97d0bda
Nour Salama <2107455@talnet.nl>
2019-12-05 15:32:59 +0100

    Opdracht 10 gemaakt

--- loops_opdrachten/loops_opdracht_10/js/script.js
+++ loops_opdrachten/loops_opdracht_10/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+
+var abc = [];
+
+abc [0] = "A";
+abc [1] = "B";
+abc [2] = "C";
+abc [3] = "D";
+abc [4] = "E";
+
+// console.log( abc[0] );
+
+for (var teller = 0 ; teller <= abc.length; teller ++){
+
+    console.log( abc [teller] );
+}
+
+
+// console.log(abc);
--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,23 @@
 //javascript code
+
+for( var teller = 1; teller <=100; teller ++) {
+    
+    if ( teller % 3 == 0 && teller % 5 == 0){ 
+    document.getElementById("resultaat").innerHTML += "<div><strong>VET COOL<strong><div/>";
+    }
+
+    if(teller % 3 == 0){
+        document.getElementById("resultaat").innerHTML += "<div><strong><div/>VET";
+    }
+
+    else if(teller % 5 == 0){
+        document.getElementById("resultaat").innerHTML += "<div><strong><div/>COOL";
+    }
+    
+    
+    else {
+        document.getElementById("resultaat").innerHTML += teller + "<br>";
+
+    }
+}
+
